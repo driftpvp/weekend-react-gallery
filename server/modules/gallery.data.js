355652113c33f8c7b801f06d258910d129902cb1
Lucas Martin <driftpvp@gmail.com>
2023-09-06 16:31:37 -0500

    added new photos and updated gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/Redford1937pickup.jpg', description: 'A red classic truck. Whose favorite color is red?', likes: 0 },
+    { id: 2, path: 'images/Greenford1937pickup.jpg', description: 'A green classic truck. Whose favorite color is green?', likes: 0 },
+    { id: 3, path: 'images/Greyford1937pickup.jpg', description: 'A grey classic truck. Whose favorite color is grey?', likes: 0 },
+    { id: 4, path: 'images/Declan.jpg', description: 'The eldest son. Favorite color red.', likes: 0 },
+    { id: 5, path: 'images/Keating.jpg', description: 'The middle son. Favorite color green.', likes: 0 },
+    { id: 6, path: 'images/Callahan.jpg', description: 'The youngest son. Favorite color grey.', likes: 0 }
 ];
 
 module.exports = galleryItems;
